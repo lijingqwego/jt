4f45cdfc835b361435ebee556e8ed5795a164b7b
lijingqwego <1169318609@qq.com>
2018-02-14 20:34:59 +0800

    商品增删改功能完成

--- jt-manage/jt-manage-controller/src/main/java/com/jt/manage/controller/ItemController.java
+++ jt-manage/jt-manage-controller/src/main/java/com/jt/manage/controller/ItemController.java
@@ -2,12 +2,14 @@ package com.jt.manage.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jt.common.vo.EasyUIResult;
 import com.jt.common.vo.SysResult;
 import com.jt.manage.pojo.Item;
+import com.jt.manage.pojo.ItemDesc;
 import com.jt.manage.service.ItemService;
 
 @RequestMapping("/item")
@@ -33,9 +35,9 @@ public class ItemController {
 	 */
 	@RequestMapping("/save")
 	@ResponseBody
-	public SysResult saveItem(Item item){
+	public SysResult saveItem(Item item,String desc){
 		try{
-			itemService.saveItem(item);
+			itemService.saveItem(item,desc);
 			log.info("新增商品成功！");
 			return SysResult.ok();
 		}catch(Exception e){
@@ -51,9 +53,9 @@ public class ItemController {
 	 */
 	@RequestMapping("/update")
 	@ResponseBody
-	public SysResult updateItem(Item item){
+	public SysResult updateItem(Item item,String desc){
 		try{
-			itemService.updateItem(item);
+			itemService.updateItem(item,desc);
 			log.info("修改商品成功！");
 			return SysResult.ok();
 		}catch(Exception e){
@@ -71,7 +73,7 @@ public class ItemController {
 	@ResponseBody
 	public SysResult deleteItem(String[] ids){
 		try{
-			itemService.deleteByIds(ids);
+			itemService.deleteItems(ids);
 			log.info("删除商品成功！");
 			return SysResult.ok();
 		}catch(Exception e){
@@ -79,4 +81,22 @@ public class ItemController {
 			return SysResult.build(201, "删除商品失败,原因："+e.getMessage());
 		}
 	}
+	
+	/**
+	 * 商品详情
+	 * @param item
+	 * @return
+	 */
+	@RequestMapping("/query/item/desc/{itemId}")
+	@ResponseBody
+	public SysResult getItemDesc(@PathVariable Long itemId){
+		ItemDesc itemDesc = itemService.getItemDesc(itemId);
+		try {
+			return SysResult.ok(itemDesc);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return SysResult.build(201, e.getMessage());
+		}
+	}
+	
 }
